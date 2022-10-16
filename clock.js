ca94a04bc0d98da518ea9630906788288cfeff31
Dero95 <84228588+Dero95@users.noreply.github.com>
2022-10-16 14:06:17 +0400

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,17 @@
+const dateInfo = new Date();
+const hr = dateInfo.getHours();
+      min = dateInfo.getMinutes();
+      sec = dateInfo.getSeconds();
+      milsec = dateInfo.getMilliseconds();
+
+var hrAngle = hr * 30 + (min * 6 / 12);
+    minAngle = min * 6 + (sec * 6 / 60);
+    secAngle = sec * 6 + (milsec * 0.36 / 1000);
+
+    function setAngle(wrapper, angle) {
+        document.querySelector('.' + wrapper).style.transform = 'rotate(' + angle + 'deg)'; 
+    }
+
+    setAngle('hr-wrapper', hrAngle);
+    setAngle('min-wrapper', minAngle);
+    setAngle('sec-wrapper', secAngle);
